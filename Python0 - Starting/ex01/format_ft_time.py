a5513c6d48aa8e483b29c6d48da447bad6f1b199
Hady <hady_awayda@hotmail.com>
2025-03-11 00:01:48 +0200

    Updated ex01

--- Python0 - Starting/ex01/format_ft_time.py
+++ Python0 - Starting/ex01/format_ft_time.py
@@ -1,15 +1,11 @@
 import time
 import datetime
 
-# Get current time in seconds since January 1, 1970
 current_time = time.time()
 
-# Format the seconds in both standard and scientific notation
 formatted_time = f"Seconds since January 1, 1970: {current_time:,.4f} or {current_time:.2e} in scientific notation"
 
-# Get the current date and format it
 current_date = datetime.datetime.now().strftime("%b %d %Y")
 
-# Print results
 print(formatted_time)
 print(current_date)
